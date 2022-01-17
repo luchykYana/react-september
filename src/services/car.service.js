bfc3ccb1b30905a35c91f9aa68ad9114e860ec8a
luchykYana <yanaluchyk@gmail.com>
2022-01-17 15:55:56 +0200

    hw_joi

--- src/services/car.service.js
+++ src/services/car.service.js
@@ -6,8 +6,7 @@ export const carService = {
     create: (car) => axiosCarService.post(urls.cars, car),
 
     getAll: () => axiosCarService.get(urls.cars),
-//     getById: axiosCarService.get(),
-//
+
     updateById: (id, car) => axiosCarService.patch(`${urls.cars}/${id}`, car),
 
     deleteById: (id) => axiosCarService.delete(`${urls.cars}/${id}`)
